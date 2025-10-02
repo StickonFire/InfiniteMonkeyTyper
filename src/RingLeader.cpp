1eeccc407af04bf4e03e7e336f74724eedc061a9
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-02 16:04:54 -0700

    Added several RingLeader function prototypes
    Added prototypes for the following functions:
     - RingLeader::runNCharactesr
     - RingLeader::listInfo
     -RingLeader::pauseMonkeyTyper
     - RingLeader::unpauseMonkeyTyper
     - RingLeader::removeMonkeyTyper

---
+++ src/RingLeader.cpp
@@ -0,0 +1,23 @@
+#include "RingLeader.hpp"
+#include "MonkeyTyper.hpp"
+
+vector<MonkeyTyperStatus> RingLeader::runNCharacters(int n){
+    return vector<MonkeyTyperStatus>();
+}
+
+vector<IdAndListInfo> RingLeader::listInfo(){
+    return vector<IdAndListInfo>();
+}
+
+
+void RingLeader::pauseMonkeyTyper(int id){
+
+}
+
+void RingLeader::unpauseMonkeyTyper(int id){
+
+}
+
+void RingLeader::removeMonkeyTyper(int id){
+
+}
