370b4b62bc0f6fd9c32450c0c15bf5f8998cfcd2
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-02 16:25:20 -0700

    Untested Implementations for RingLeader functions
    Added a couple untested implementations for RingLeader functions. These are for the streamInfo, promptInfo, and createMonkeyTyper functions.

--- src/RingLeader.cpp
+++ src/RingLeader.cpp
@@ -9,6 +9,22 @@ vector<IdAndListInfo> RingLeader::listInfo(){
     return vector<IdAndListInfo>();
 }
 
+StreamInfo RingLeader::streamInfo(int id){
+    if(typers.find(id) != typers.end())
+        return typers[id].streamInfo();
+}
+
+PromptInfo RingLeader::promptInfo(int id){
+    if(typers.find(id) != typers.end())
+        return typers[id].promptInfo();
+}
+
+int RingLeader::createMonkeyTyper(std::string query, int seed){
+    unique_ptr<LetterSelector> selector = make_unique<mt19937LetterSelector>(seed);
+    int id = idGenerator->generateId();
+    this->typers[id] = MonkeyTyper(id,std::move(selector),query);
+    return id;
+}
 
 void RingLeader::pauseMonkeyTyper(int id){
 
