398360a9222cfbed53237ac83f80ea8cbefb9477
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-14 01:59:23 -0700

    Changed used_ids to camel case
    Changed ussed_ids in RingLeaderTest.cpp to usedIds to better conform to coding standards set up.

--- test/RingLeaderTest.cpp
+++ test/RingLeaderTest.cpp
@@ -5,8 +5,8 @@
 #include <gtest/gtest.h>
 TEST(RingLeaderWholisticTest,EmptyList){
     std::map<int,MonkeyTyper> empty;
-    std::set<int> used_ids;
-    unique_ptr<CounterIdMaker> idGenerator = make_unique<CounterIdMaker>(50,used_ids);
+    std::set<int> usedIds;
+    unique_ptr<CounterIdMaker> idGenerator = make_unique<CounterIdMaker>(50,usedIds);
     RingLeader test(empty,std::move(idGenerator));
     std::vector<ListInfo> expectedResult;
     EXPECT_EQ(test.listInfo(),expectedResult);
