55b3ffe60cb64b5e199b7c810d9e51a5063bed0c
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-30 14:59:26 -0700

    Added tests for MonkeyTyperFactory
    Added two Tests, one for each MonkeyTyperFactory instance. This is the mt19937MonkeyTyperFactory and the MockMonkeyTyperFactory.

---
+++ test/MonkeyTyperFactoryTest.cpp
@@ -0,0 +1,69 @@
+#include "MonkeyTyperFactory.hpp"
+#include "MonkeyTyper.hpp"
+#include "mockClasses.hpp"
+
+#include <random>
+
+#include "gtest/gtest.h"
+
+using ::testing::Return;
+using ::testing::Mock;
+
+/**
+ * Tests the mt19937MonkeyTyperFactory. Simply checks if the monkeyTyper is correctly set by checking its return values
+ */
+TEST(MonkeyTyperFactoryTest,mt19937Test){
+    unsigned int seed = 100;
+    std::string testQuery = "This should be larger than the query to prevent issues.";
+    int runs = testQuery.size()-1;
+    int id = 10;
+    unique_ptr<mt19937LetterSelector> controlSelector = make_unique<mt19937LetterSelector>(default_alphabet,seed);
+    MonkeyTyper control(id,std::move(controlSelector),testQuery);
+    
+    mt19937MonkeyTyperFactory factory;
+    MonkeyTyper test = factory.build(id,seed,testQuery);
+
+    control.moveStream(runs);
+    test.moveStream(runs);
+
+    EXPECT_EQ(control.listInfo(),test.listInfo());
+    EXPECT_EQ(control.streamInfo(),test.streamInfo());
+    EXPECT_EQ(control.promptInfo(),test.promptInfo());
+}
+
+/**
+ * Tests the MockMonkeyTyperFactory
+ */
+TEST(MonkeyTyperFactoryTest,MockTest){
+    int id = 125;
+    int seed = 25;
+    std::string query = "ab";
+
+    unique_ptr<MockLetterSelector> mockSelector = make_unique<MockLetterSelector>();
+    EXPECT_CALL(*mockSelector,selectCharacter())
+        .Times(2)
+        .WillOnce(Return('a'))
+        .WillOnce(Return('b'));
+    EXPECT_CALL(*mockSelector,getSeed())
+        .Times(1)
+        .WillOnce(Return(seed));
+
+    MockMonkeyTyperFactory mockFactory;
+    EXPECT_CALL(mockFactory,build(id,seed,query))
+        .Times(1)
+        .WillOnce(Return(MonkeyTyper(id,std::move(mockSelector),query)));
+
+    MonkeyTyper test = mockFactory.build(id,seed,query);
+    
+    EXPECT_EQ(Completed,test.moveStream(2));
+    vector<char> expectedPacketStream{'a','b'};
+    vector<LetterOutcome> expectedOutcome{Match,Complete};
+    vector<char> expectedCorrespondingQuery{'a','b'};
+    vector<int> expectedBestLocation{1,2};
+    ListInfo expectedInfo(id,2,2,2,expectedPacketStream,expectedOutcome,expectedCorrespondingQuery,expectedBestLocation);
+    StreamInfo expectedStreamInfo(seed,std::string("ab"),expectedInfo);
+    PromptInfo expectedPromptInfo(seed,query,expectedInfo);
+    EXPECT_EQ(test.listInfo(),expectedInfo);
+    EXPECT_EQ(test.streamInfo(),expectedStreamInfo);
+    EXPECT_EQ(test.promptInfo(),expectedPromptInfo);
+}
