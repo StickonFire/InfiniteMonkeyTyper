34c8b176cda874b12e3de154cabe64808f6b4fc6
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-30 14:37:39 -0700

    Added some includes to MonkeyTyperFactory.cpp
    Added an include to memory.h for MonkeyTyperFactory since unique_ptr is used there.

--- src/MonkeyTyperFactory.cpp
+++ src/MonkeyTyperFactory.cpp
@@ -1,6 +1,8 @@
 #include "MonkeyTyperFactory.hpp"
 #include "MonkeyTyper.hpp"
 
+#include <memory>
+
 MonkeyTyper mt19937MonkeyTyperFactory::build(int id, unsigned int seed, std::string query){
     unique_ptr<mt19937LetterSelector> selector = make_unique<mt19937LetterSelector>(seed);
     return MonkeyTyper(id,std::move(selector),query);
