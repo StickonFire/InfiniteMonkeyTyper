2f1b3b3595359faeef650e8dce20dce38d650acf
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-10-30 14:41:08 -0700

    Fixed the selector creation in build function
    Corrected the mt19937LetterSelector selector's constructor's argument list. Now it initializes the selector's alphabet to default_alphabet.

--- src/MonkeyTyperFactory.cpp
+++ src/MonkeyTyperFactory.cpp
@@ -4,6 +4,6 @@
 #include <memory>
 
 MonkeyTyper mt19937MonkeyTyperFactory::build(int id, unsigned int seed, std::string query){
-    unique_ptr<mt19937LetterSelector> selector = make_unique<mt19937LetterSelector>(seed);
+    unique_ptr<mt19937LetterSelector> selector = make_unique<mt19937LetterSelector>(default_alphabet,seed);
     return MonkeyTyper(id,std::move(selector),query);
 }
