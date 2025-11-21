6a4e5b660d1f663acaedcf4cc4c7053327deab33
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-11-20 21:06:05 -0800

    ModelInfo.hpp filled in
    Filled in the declarations of the structs TyperInfo and ModelInfo.

--- include/ModelInfo.hpp
+++ include/ModelInfo.hpp
@@ -1,10 +1,25 @@
 #ifndef MODELINFO
 #define MODELINFO
 
+#include "MonkeyTyper.hpp"
+
 #include <map>
+#include <string>
 
-class ModelInfo{
+struct TyperInfo{
+    ListInfo listInfo;
+    std::string stream;
+    std::string prompt;
+    unsigned int seed;
 
+    TyperInfo(ListInfo& listInfo, std::string stream, std::string prompt, unsigned int seed);
+};
+
+struct ModelInfo{
+    std::map<int,std::string> messages;
+    std::map<int,TyperInfo> typerValues;
+
+    ModelInfo(std::map<int,std::string> &messages,std::map<int,TyperInfo> &typerValues);
 };
 
 #endif
