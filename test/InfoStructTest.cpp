bce7bf26a76e00e74276d75dc784e4c2d52e60f7
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-11-25 13:14:04 -0800

    Implement TyperInfo tests
    Implemented the tests for TyperInfo.
    These include:
     - Correct: Directly compares two TypersInfo with the same values.
     - CycleThroughIncorrect: moves through each TyperInfo field, changing them to be unequal before resetting them to be equal for the next field.

--- test/InfoStructTest.cpp
+++ test/InfoStructTest.cpp
@@ -70,3 +70,46 @@ TEST(ListInfoEquality,CycleThroughIncorrect){
     second.packetBestGuessLocation.push_back(1);
     EXPECT_FALSE(first == second);
 }
+
+class TyperInfoEqualityTestSuite : public testing::Test {
+
+    protected:
+        TyperInfoEqualityTestSuite(): empty(), first(empty,"95","65",40), second(empty,"95","65",40){ }
+        ListInfo empty;
+        TyperInfo first;
+        TyperInfo second;
+};
+
+TEST_F(TyperInfoEqualityTestSuite,Correct){
+    EXPECT_EQ(first,second);
+}
+
+TEST_F(TyperInfoEqualityTestSuite,CycleThroughIncorrect){
+    std::vector<char> packetStream{'a'};
+    std::vector<LetterOutcome> packetOutcome{Match};
+    std::vector<char> packetCorresponding{'a'};
+    std::vector<int> packetLocation{1};
+    ListInfo different(10,10,10,10,packetStream,packetOutcome,packetCorresponding,packetLocation);
+    first.listInfo = ListInfo(different);
+    EXPECT_FALSE(first == second) << "TyperInfo ListInfo different but incorrectly marked equal";
+    second.listInfo = ListInfo(different);
+    EXPECT_EQ(first,second) << "TyperInfo ListInfos are equal, but incorrectly marked unequal";
+
+    std::string replacementPrompt = "replacementPrompt";
+    first.prompt = replacementPrompt;
+    EXPECT_FALSE(first == second) << "TyperInfo prompts are unequal, but declared equal";
+    second.prompt = replacementPrompt;
+    EXPECT_EQ(first,second) << "TyperInfo prompts are equal, but declared unequal";
+
+    std::string replacementStream = "replacementStream";
+    first.stream = replacementStream;
+    EXPECT_FALSE(first == second) << "TyperInfo streams are unequal, but declared equal";
+    second.stream = replacementStream;
+    EXPECT_EQ(first,second) << "TyperInfo streams are equal, but declared unequal";
+
+    unsigned int replacementSeed = 34;
+    first.seed = replacementSeed;
+    EXPECT_FALSE(first == second) << "TyperInfo seeds are unequal, but declared equal";
+    second.seed = replacementSeed;
+    EXPECT_EQ(first,second) << "TyperInfo seeds are equal, but declared unequal";
+}
