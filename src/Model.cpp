0052d97c4c9b6a779a064b6270c7e7bf58eea756
StickonFire <70039286+StickonFire@users.noreply.github.com>
2026-02-17 14:07:39 -0800

    Implemented Model constructor and getters
    Implemented the Model's constructor and getters. Functions included are:
     - Model::Model(std::unique_ptr<RingLeader>, int)
     - Model::getRunSize()
     - Model::setRunSize(int)
     - Model::modelInfo()

--- src/Model.cpp
+++ src/Model.cpp
@@ -2,3 +2,20 @@
 #include "RingLeader.hpp"
 #include "Model.hpp"
 
+Model::Model(std::unique_ptr<RingLeader> ringLeader, int runSize): ringLeader(std::move(ringLeader)), runSize(runSize) { }
+
+ModelInfo Model::modelInfo(){
+    std::map<int,std::string> placeHolderMessages;
+    std::map<int,TyperInfo> typerInfos = this->ringLeader->typerInfo();
+    ModelInfo result(placeHolderMessages,typerInfos);
+    return result;
+}
+
+int Model::getRunSize(){
+    return this->runSize;
+}
+
+void Model::setRunSize(int runSize){
+    this->runSize = runSize;
+}
+
