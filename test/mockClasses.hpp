463592a81129ac201f87a55ca9367c773212b7ab
StickonFire <70039286+StickonFire@users.noreply.github.com>
2026-03-11 22:25:27 -0700

    Added MockModel class
    Added the MockModel class. This is a Mock of the AbstractModel. It will be used to test the ModelRequests.

--- test/mockClasses.hpp
+++ test/mockClasses.hpp
@@ -1,13 +1,14 @@
 #ifndef MOCKCLASS
 #define MOCKCLASS
 
-#include <random>
-#include <gmock/gmock.h>
-#include <iterator>
-
 #include "MonkeyTyper.hpp"
 #include "MonkeyTyperFactory.hpp"
 #include "RingLeader.hpp"
+#include "Model.hpp"
+
+#include <random>
+#include <gmock/gmock.h>
+#include <iterator>
 using namespace std;
 
 class MockLetterSelector : public LetterSelector {
@@ -26,6 +27,18 @@ class MockIdMaker : public IdMaker {
 class MockMonkeyTyperFactory : public MonkeyTyperFactory {
     public:
         MOCK_METHOD(MonkeyTyper,build, (int id, unsigned int seed, std::string query), (override));
+};
+
+class MockModel : public AbstractModel {
+    public:
+        MOCK_METHOD(void,doRun,(),(override));
+        MOCK_METHOD(void,doCreateMonkeyTyper,(std::string query,unsigned int seed),(override));
+        MOCK_METHOD(void,doRemoveMonkeyTyper,(unsigned int id),(override));
+        MOCK_METHOD(void,doPauseMonkeyTyper,(unsigned int id),(override));
+        MOCK_METHOD(void,doUnpauseMonkeyTyper,(unsigned int id),(override));
+        MOCK_METHOD(ModelInfo,modelInfo,(),(override));
+        MOCK_METHOD(int,getRunSize,(),(override));
+        MOCK_METHOD(void,setRunSize,(int runSize),(override));
 };
 
 #endif
